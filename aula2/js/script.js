48aa4f0670e5a5518fc2d5cedf10791b480a3074
milani <fandangoslord2@gmail.com>
2024-03-26 10:20:24 -0300

    aula3

--- aula2/js/script.js
+++ aula2/js/script.js
@@ -1,3 +1,17 @@
 alerta = () => {
-    alert("Você foi avisado!")
-}
+  alert("Você foi avisado!");
+};
+
+const enviar = () => {
+  let nome = document.getElementById("nome").value;
+
+  document.getElementById(
+    "resposta"
+  ).innerHTML = `<h2>Olá ${nome}, formulário enviado com sucesso!</h2>`;
+
+  if (nome == "") {
+    alert("Bem Vindo!");
+  } else {
+    alert(`Olá ${nome}, bem vindo!`);
+  }
+};
