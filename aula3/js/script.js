550784c42e9a2600eb39494f9eb52d85928890e6
milani <fandangoslord2@gmail.com>
2024-06-12 15:15:18 -0300

    aaa

--- aula3/js/script.js
+++ aula3/js/script.js
@@ -7,6 +7,8 @@ trocaTema = () => {
   const isLight = document.documentElement.classList.contains("light");
 
 const imagem = isLight ? "./img/rg.jpg" : "./img/rg2.jpg"
-
 document.querySelector("#perfil img").setAttribute("src", imagem)
+
+const alt = isLight ? "Um homem de máscara com a mão no rosto, vestido com a camisa do al-rayyan" : "Um homem com a mão no rosto, vestindo a camisa do corinthians, e calvo"
+document.querySelector("#perfil img").setAttribute("alt", alt)
 };
